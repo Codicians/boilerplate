1361b7f61817710ecbf9699271b1c24550e7d3e4
d8vjork <ruben_robles97@hotmail.com>
2017-06-11 17:03:06 +0000

    Apply fixes from StyleCI

--- src/Boilerplate/Boilerplate.php
+++ src/Boilerplate/Boilerplate.php
@@ -5,165 +5,161 @@ namespace Boilerplate;
 use BoilerplateInterface;
 
 /**
- * Boilerplate class for templates setup
- * 
- * @package Boilerplate
+ * Boilerplate class for templates setup.
  */
-class Boilerplate implements BoilerplateInterface {
+class Boilerplate implements BoilerplateInterface
+{
+    /**
+     * @var string
+     */
+    protected $namespace = '';
 
-	/**
-	 * @var string
-	 */
-	protected $namespace = '';
+    /**
+     * @var array
+     */
+    protected $formats = [];
 
-	/**
-	 * @var array
-	 */
-	protected $formats = [];
+    /**
+     * @var array
+     */
+    protected $menus = [];
 
-	/**
-	 * @var array
-	 */
-	protected $menus = [];
+    /**
+     * @var array
+     */
+    protected $sidebars = [];
 
-	/**
-	 * @var array
-	 */
-	protected $sidebars = [];
+    /**
+     * @var array
+     */
+    protected $support = [];
 
-	/**
-	 * @var array
-	 */
-	protected $support = [];
+    /**
+     * Setup the class instance.
+     *
+     * @param string $name
+     * @param array  $params
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function setup($namespace, array $params)
+    {
+        $this->namespace = $namespace;
 
-	/**
-	 * Setup the class instance
-	 *
-	 * @param string $name
-	 * @param array $params
-	 * 
-	 * @since 0.1.0
-	 * 
-	 * @return void
-	 */
-	public function setup($namespace, array $params)
-	{
-		$this->namespace 	= $namespace;
+        if (!empty($params)) {
+            if (!empty($params['formats'])) {
+                $this->formats = (array) $params['formats'];
+            }
 
-		if (!empty($params)) {
+            if (!empty($params['menus'])) {
+                $this->menus = (array) $params['menus'];
+            }
 
-			if (!empty($params['formats'])) {
-				$this->formats = (array) $params['formats'];
-			}
+            if (!empty($params['support'])) {
+                $this->support = (array) $params['support'];
+            }
+        }
 
-			if (!empty($params['menus'])) {
-				$this->menus = (array) $params['menus'];
-			}
+        $this->setup_theme();
+    }
 
-			if (!empty($params['support'])) {
-				$this->support = (array) $params['support'];
-			}
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    protected function setup_theme()
+    {
+        // Load text domain for language translations - disabled
+        // load_theme_textdomain($this->namespace, get_template_directory() . '/languages');
 
-		}
+        // Register navigation menus
+        register_nav_menus($this->menus);
 
-		$this->setup_theme();
-	}
+        // Register sidebars
+        register_sidebars(count($this->sidebars), $this->sidebars);
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @return void
-	 */
-	protected function setup_theme()
-	{
-		// Load text domain for language translations - disabled
-		// load_theme_textdomain($this->namespace, get_template_directory() . '/languages');
+        // Modify theme support with new array values if you want
+        add_theme_support('post-formats', $this->formats);
 
-		// Register navigation menus
-		register_nav_menus($this->menus);
+        // Add other theme support
+        foreach ($this->support as $value) {
+            add_theme_support($value);
+        }
 
-		// Register sidebars
-		register_sidebars(count($this->sidebars), $this->sidebars);
+        $this->setup_hooks();
+    }
 
-		// Modify theme support with new array values if you want
-		add_theme_support('post-formats', $this->formats);
+    /**
+     * Setup WordPress hooks for adding.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    protected function setup_hooks()
+    {
+        add_action('after_setup_theme', [$this, 'setup']);
+    }
 
-		// Add other theme support
-		foreach ($this->support as $value) {
-			add_theme_support($value);
-		}
+    /**
+     * Add a menu to the instance.
+     *
+     * @since 0.1.0
+     *
+     * @param mixed $id
+     * @param mixed $text
+     *
+     * @return void
+     */
+    public function add_menu($id, $text)
+    {
+        $this->menus[$id] = $text;
+    }
 
-		$this->setup_hooks();
-	}
+    /**
+     * Add an array of support features to the instance.
+     *
+     * @since 0.1.0
+     *
+     * @param array $support
+     *
+     * @return void
+     */
+    public function add_support(array $support)
+    {
+        $this->support = array_merge($this->support, $support);
+    }
 
-	/**
-	 * Setup WordPress hooks for adding 
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @return void
-	 */
-	protected function setup_hooks()
-	{
-		add_action('after_setup_theme', array($this, 'setup'));
-	}
+    /**
+     * Add an array of sidebars to the instance.
+     *
+     * @since 0.1.0
+     *
+     * @param array $sidebar
+     *
+     * @return void
+     */
+    public function add_sidebar(array $sidebar)
+    {
+        $this->sidebars = array_merge($this->menus, $sidebar);
+    }
 
-	/**
-	 * Add a menu to the instance
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @param mixed $id
-	 * @param mixed $text
-	 * 
-	 * @return void
-	 */
-	public function add_menu($id, $text)
-	{
-		$this->menus[$id] = $text;
-	}
-
-	/**
-	 * Add an array of support features to the instance
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @param array $support
-	 * @return void
-	 */
-	public function add_support(array $support)
-	{
-		$this->support = array_merge($this->support, $support);
-	}
-
-	/**
-	 * Add an array of sidebars to the instance
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @param array $sidebar
-	 * 
-	 * @return void
-	 */
-	public function add_sidebar(array $sidebar)
-	{
-		$this->sidebars = array_merge($this->menus, $sidebar);
-	}
-
-	/**
-	 * Add an array of formats to the instance
-	 *
-	 * @since 0.1.0
-	 * 
-	 * @param array $formats
-	 * 
-	 * @return void
-	 */
-	public function add_format(array $formats)
-	{
-		$this->formats = array_merge($this->formats, $formats);
-	}
-
+    /**
+     * Add an array of formats to the instance.
+     *
+     * @since 0.1.0
+     *
+     * @param array $formats
+     *
+     * @return void
+     */
+    public function add_format(array $formats)
+    {
+        $this->formats = array_merge($this->formats, $formats);
+    }
 }
--- src/Boilerplate/BoilerplateInterface.php
+++ src/Boilerplate/BoilerplateInterface.php
@@ -3,67 +3,65 @@
 namespace Boilerplate;
 
 /**
- * Interface of the main class
- * 
- * @package Boilerplate
+ * Interface of the main class.
  */
 interface BoilerplateInterface
 {
-	/**
-	 * Setup the class instance
-	 *
-	 * @return void
-	 */
-	public function setup();
+    /**
+     * Setup the class instance.
+     *
+     * @return void
+     */
+    public function setup();
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features
-	 *
-	 * @return void
-	 */
-	public function setup_theme();
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @return void
+     */
+    public function setup_theme();
 
-	/**
-	 * Setup WP hooks for the template
-	 *
-	 * @return void
-	 */
-	public function setup_hooks();
+    /**
+     * Setup WP hooks for the template.
+     *
+     * @return void
+     */
+    public function setup_hooks();
 
-	/**
-	 * Add a menu to the instance
-	 *
-	 * @param mixed $id
-	 * @param mixed $text
-	 * 
-	 * @return void
-	 */
-	public function add_menu($id, $text);
+    /**
+     * Add a menu to the instance.
+     *
+     * @param mixed $id
+     * @param mixed $text
+     *
+     * @return void
+     */
+    public function add_menu($id, $text);
 
-	/**
-	 * Add an array of support features to the instance
-	 *
-	 * @param array $support
-	 * 
-	 * @return void
-	 */
-	public function add_support(array $support);
+    /**
+     * Add an array of support features to the instance.
+     *
+     * @param array $support
+     *
+     * @return void
+     */
+    public function add_support(array $support);
 
-	/**
-	 * Add an array of sidebars to the instance
-	 *
-	 * @param array $sidebar
-	 * 
-	 * @return void
-	 */
-	public function add_sidebar(array $sidebar);
+    /**
+     * Add an array of sidebars to the instance.
+     *
+     * @param array $sidebar
+     *
+     * @return void
+     */
+    public function add_sidebar(array $sidebar);
 
-	/**
-	 * Add an array of formats to the instance
-	 *
-	 * @param array $formats
-	 * 
-	 * @return void
-	 */
-	public function add_format(array $formats);
+    /**
+     * Add an array of formats to the instance.
+     *
+     * @param array $formats
+     *
+     * @return void
+     */
+    public function add_format(array $formats);
 }
